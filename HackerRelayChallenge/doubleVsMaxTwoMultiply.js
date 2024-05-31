26ed4ec6ab974f3f9681af5584d8b62372f76730
AkiMadie16 <madie86@gmail.com>
2024-05-31 11:48:37 +0930

    sumMultiplier modified code

--- HackerRelayChallenge/doubleVsMaxTwoMultiply.js
+++ HackerRelayChallenge/doubleVsMaxTwoMultiply.js
@@ -2,8 +2,17 @@ function multiplier(arr) {
 const DoubleOfTotal = arr.reduce((prev,cur) => prev + cur, 0)
 
 // assumption - Largest is arr first element
-const Largest = arr[0]
+const Largest = arr.sort((a,b) => a > b ? -1 : 0)
 
-return arr.filter((num, i) => Largest > num[i] && num[i] * num[i+1] > DoubleOfTotal) ? true : false 
+return (Largest[0] * Largest[1]  > DoubleOfTotal) ? true : false 
 }
 console.log(multiplier([2,2,2,2,4,1]))
+
+//  Sum Multiplier
+// Have the function SumMultiplier(arr) take the array of numbers stored in arr and return the string true if any two numbers can be multiplied so that the answer is greater than double the sum of all the elements in the array. If not, return the string false. For example: if arr is [2, 5, 6, -6, 16, 2, 3, 6, 5, 3] then the sum of all these elements is 42 and doubling it is 84. There are two elements in the array, 16 * 6 = 96 and 96 is greater than 84, so your program should return the string true.
+
+// Input: [2, 2, 2, 2, 4, 1]
+// Output: false
+
+// Input: [1, 1, 2, 10, 3, 1, 12]
+// Output: true
